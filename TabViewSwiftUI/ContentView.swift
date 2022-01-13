760f7ef75fde058ffc7da98651fd3eb502bfc34c
mocchan <hcmnorgir0808@yahoo.co.jp>
2022-01-13 23:53:42 +0900

    Initial Commit

---
+++ TabViewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabViewSwiftUI
+//
+//  Created by 岩本康孝 on 2022/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabViewSwiftUI/TabViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSwiftUIApp.swift
+//  TabViewSwiftUI
+//
+//  Created by 岩本康孝 on 2022/01/13.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
